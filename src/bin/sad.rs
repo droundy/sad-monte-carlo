18900d91ed9599ea3f2f8d7df2a7e55bb66ae30b
David Roundy <roundyd@physics.oregonstate.edu>
2018-07-31 16:30:11 -0700

    add diagnostic print after (short) simulation

--- src/bin/sad.rs
+++ src/bin/sad.rs
@@ -10,4 +10,7 @@ fn main() {
     for _ in 0..1000 {
         sad.move_once();
     }
+    println!("Rejected {}/{} = {}% of the moves",
+             sad.rejected_moves, sad.moves,
+             100.0*sad.rejected_moves as f64/sad.moves as f64);
 }
