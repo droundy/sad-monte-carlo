c9d6f454d690a21ab3665251685793bfc9fc0107
David Roundy <roundyd@physics.oregonstate.edu>
2018-08-02 16:28:22 -0700

    add benchmarking of SW move_once

--- benches/bench-sad.rs
+++ benches/bench-sad.rs
@@ -9,12 +9,23 @@ use criterion::Criterion;
 use sadmc::mc::MonteCarlo;
 use sadmc::mc::sad::{Sad, SadParams};
 use sadmc::system::square::{SquareWell, SquareWellNParams};
+use sadmc::system::{Length, MovableSystem};
+
+fn gen_sw(n_atoms: usize) -> SquareWell {
+    let mut sw_params = SquareWellNParams::default();
+    sw_params.N = n_atoms;
+    let mut sw = SquareWell::from(sw_params);
+    // Randomize things a bit before beginning.
+    let mut rng = sadmc::rng::MyRng::from_u64(1);
+    for _ in 0..n_atoms*1000 {
+        sw.move_once(&mut rng, Length::new(1.0));
+    }
+    sw
+}
 
 fn gen_sad(n_atoms: usize) -> Sad<SquareWell> {
     let sad_params = SadParams::default();
-    let mut sw_params = SquareWellNParams::default();
-    sw_params.N = n_atoms;
-    let mut sad = Sad::<SquareWell>::from_params(sad_params, SquareWell::from(sw_params),
+    let mut sad = Sad::<SquareWell>::from_params(sad_params, gen_sw(n_atoms),
                                                  ::std::path::PathBuf::from("/dev/null"));
     // Randomize things a bit before beginning.
     for _ in 0..n_atoms*1000 {
@@ -33,6 +44,16 @@ fn criterion_benchmark(c: &mut Criterion) {
                                  move |b, &&n_atoms| {
                                      let mut sad = gen_sad(n_atoms);
                                      b.iter(|| sad.move_once())
+                                 },
+                                 &[50, 100,
+                                   // 200, 400
+                                   ]);
+
+    c.bench_function_over_inputs("SW_sw_move_once",
+                                 move |b, &&n_atoms| {
+                                     let mut sw = gen_sw(n_atoms);
+                                     let mut rng = sadmc::rng::MyRng::from_u64(2);
+                                     b.iter(|| sw.move_once(&mut rng, Length::new(0.1)))
                                  },
                                  &[50, 100,
                                    // 200, 400
