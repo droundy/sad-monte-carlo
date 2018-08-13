cc7e257f3c56868996aa98c4cac07bc0418ad237
David Roundy <roundyd@physics.oregonstate.edu>
2018-08-13 05:54:50 -0700

    movie of several

--- plotting/movie.py
+++ plotting/movie.py
@@ -4,8 +4,14 @@ import yaml, sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.ion()
+allcolors = ['g','r','b',]
 
+my_energy = {}
+my_histogram = {}
+my_entropy = {}
+my_time = {}
+my_color = {}
+max_iter = 0
 for fname in sys.argv[1:]:
     print(fname)
     with open(fname) as f:
@@ -16,27 +22,48 @@ for fname in sys.argv[1:]:
     print('bins:', data['bins'].keys())
     print('movies:', data['movies'].keys())
     print(data.keys())
-    energy = np.array(data['movies']['energy'])
-    time = np.array(data['movies']['time'])
-    S = np.array(data['movies']['entropy'])
-    histogram = np.array(data['movies']['histogram'])
+    my_color[fname] = allcolors.pop()
+    my_energy[fname] = np.array(data['movies']['energy'])
+    my_time[fname] = np.array(data['movies']['time'])
+    if len(my_time[fname]) > max_iter:
+        max_iter = len(my_time[fname])
+    my_entropy[fname] = np.array(data['movies']['entropy'])
+    my_histogram[fname] = np.array(data['movies']['histogram'])
 
-    print(energy.shape, S.shape)
+plt.ion()
+for i in range(max_iter):
+    plt.figure('Histogram')
+    plt.cla()
+    plt.figure('Entropy')
+    plt.cla()
+    for fname in my_energy.keys():
+        if i < len(my_time[fname]):
+            t = my_time[fname][i]
+            plt.figure('Entropy')
+            if i == 0:
+                plt.plot(my_energy[fname], my_entropy[fname][i,:], my_color[fname],
+                         label=fname)
+            else:
+                plt.plot(my_energy[fname], my_entropy[fname][i-1,:], my_color[fname],
+                         alpha=0.5)
+                plt.plot(my_energy[fname], my_entropy[fname][i,:], my_color[fname])
+            plt.title('$t=%.3g/%.3g' % (t, my_time[fname][-1]))
+            plt.ylabel('$S$')
+            plt.legend(loc='best')
 
-    for i,t in enumerate(time):
-        plt.figure('Entropy')
-        plt.plot(energy, S[i,:], label='t=%g'%t)
-        plt.title('$t=%.3g/%.3g' % (t, time[-1]))
-        plt.ylabel('$S$')
-        # plt.legend(loc='best')
-
-        plt.figure('Histogram')
-        plt.title('$t=%.3g/%.3g' % (t, time[-1]))
-        plt.ylabel('histogram')
-        plt.plot(energy, histogram[i,:], label='t=%g'%t)
-        #plt.legend(loc='best')
-        plt.pause(3.0)
-        print('on time', t)
+            plt.figure('Histogram')
+            plt.title('$t=%.3g/%.3g' % (t, my_time[fname][-1]))
+            plt.ylabel('histogram')
+            if i == 0:
+                plt.plot(my_energy[fname], my_histogram[fname][i,:], my_color[fname],
+                         label=fname)
+            else:
+                plt.plot(my_energy[fname], my_histogram[fname][i-1,:], my_color[fname],
+                         alpha=0.5)
+                plt.plot(my_energy[fname], my_histogram[fname][i,:], my_color[fname])
+            plt.legend(loc='best')
+    plt.pause(3.0)
+    print('on time', t)
 
 plt.ioff()
 plt.show()
