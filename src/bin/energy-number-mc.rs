eee700b14a36b4835298d882f02bc047aca7e04f
David Roundy <roundyd@physics.oregonstate.edu>
2018-09-21 05:44:52 -0700

    add binary for energy-number-mc

--- src/bin/energy-number-mc.rs
+++ src/bin/energy-number-mc.rs
@@ -3,10 +3,10 @@ extern crate sadmc;
 use sadmc::system::optsquare::*;
 
 use sadmc::mc::MonteCarlo;
-use sadmc::mc::energy::EnergyMC;
+use sadmc::mc::energy_number::EnergyNumberMC;
 
 fn main() {
-    let mut mc = EnergyMC::<SquareWell>::from_args::<SquareWellNParams>();
+    let mut mc = EnergyNumberMC::<SquareWell>::from_args::<SquareWellParams>();
     loop {
         mc.move_once();
     }
