1fc7239e09155103cf87f4284c82cd689ceb7bf8
David Roundy <roundyd@physics.oregonstate.edu>
2018-10-05 05:22:51 -0700

    reorder to prioritize 1e7 square, and add tail to submit-square.sh

--- submit-square.sh
+++ submit-square.sh
@@ -6,10 +6,13 @@ cargo build --release
 
 rq run -R -J sad_1000 target/release/energy-mc --N 1000 --filling-fraction 0.22 --sad-min-T 0.1 --well-width 1.3 --movie-time '10^(1/4)' --save-as sad_1000.yaml --translation-scale 0.05 --save-time 0.5
 
+rq run -R -J samc-1e7_1000 target/release/energy-mc --N 1000 --filling-fraction 0.22 --samc-t0 1e7 --well-width 1.3 --movie-time '10^(1/4)' --save-as samc-1e7_1000.yaml --translation-scale 0.05 --save-time 0.5
+
 rq run -R -J samc-1e9_1000 target/release/energy-mc --N 1000 --filling-fraction 0.22 --samc-t0 1e9 --well-width 1.3 --movie-time '10^(1/4)' --save-as samc-1e9_1000.yaml --translation-scale 0.05 --save-time 0.5
 
 rq run -R -J samc-1e8_1000 target/release/energy-mc --N 1000 --filling-fraction 0.22 --samc-t0 1e8 --well-width 1.3 --movie-time '10^(1/4)' --save-as samc-1e8_1000.yaml --translation-scale 0.05 --save-time 0.5
 
-rq run -R -J samc-1e7_1000 target/release/energy-mc --N 1000 --filling-fraction 0.22 --samc-t0 1e7 --well-width 1.3 --movie-time '10^(1/4)' --save-as samc-1e7_1000.yaml --translation-scale 0.05 --save-time 0.5
-
 rq run -R -J wl_1000 target/release/energy-mc --N 1000 --filling-fraction 0.22 --wl --well-width 1.3 --movie-time '10^(1/4)' --save-as wl_1000.yaml --translation-scale 0.05 --save-time 0.5
+
+sleep 3
+tail -f *1000.out
