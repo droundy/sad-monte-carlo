768324242d7f953205a2c5a8248da47dd67c3fce
David Roundy <roundyd@physics.oregonstate.edu>
2018-10-05 05:23:32 -0700

    make grand smaller

--- submit-grand.sh
+++ submit-grand.sh
@@ -4,4 +4,4 @@ set -ev
 
 cargo build --release
 
-rq run -R -J grand-samc-1e9 target/release/energy-number-mc --samc-t0 1e9 --well-width 1.3 --save-as grand-samc-1e9.yaml --save-time 0.5 --cell-volume 1000 --acceptance-rate 0.5
+rq run -R -J grand-samc-1e9 target/release/energy-number-mc --samc-t0 1e9 --well-width 1.3 --save-as grand-samc-1e9.yaml --save-time 0.5 --cell-volume 125 --acceptance-rate 0.5
