8d49e6bdb21b3831263959abbefca14ea952f97a
David Roundy <roundyd@physics.oregonstate.edu>
2018-10-15 12:19:58 -0700

    add assertions on energy when saving movie (reveals bug quickly)

--- src/mc/number.rs
+++ src/mc/number.rs
@@ -616,8 +616,10 @@ impl Movies {
     }
 }
 impl<S: GrandSystem> Plugin<NumberMC<S>> for Movies {
-    fn run(&self, mc: &NumberMC<S>, _sys: &S) -> plugin::Action {
+    fn run(&self, mc: &NumberMC<S>, sys: &S) -> plugin::Action {
         if let Some(movie_time) = self.movie_time {
+            assert_eq!(sys.energy(), sys.compute_energy());
+            assert!(sys.energy() <= Energy::new(0.0));
             let moves = mc.num_moves();
             if plugin::TimeToRun::TotalMoves(moves) == self.period.get() {
                 println!("Saving movie...");
