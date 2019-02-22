ef4c896b39cba42e0f0b1034b6e2eaa5d68f3ff0
Jin Kiatvongcharoen <kiatvonj@oregonstate.edu>
2019-02-22 10:46:41 -0800

    Working on how to use systems for dynein

--- src/system/dynein.rs
+++ src/system/dynein.rs
@@ -8,10 +8,11 @@ use rand::prelude::*;
 #[derive(Serialize, Deserialize, Debug, ClapMe)]
 #[allow(non_snake_case)]
 pub struct DyneinParams {
-    /// Leg Angles
+    /// Motor Domain Angles
     nm: usize,
     fm: usize,
-    l: usize
+    ///Initial Displacement
+    initd: usize,
 
 }
 
@@ -21,20 +22,26 @@ pub struct DyneinParams {
 pub struct Dynein {
     /// The energy of the system
     E: Energy,
-    /// The leg angles
+    /// The unbinding rates
+    K: f64,
+    /// The motor domain angles
     pub nm: usize,
     pub fm: usize,
-    /// The displacement of foot domain
-    pub l: usize,
+    /// The initial displacement of foot domain
+    pub initd: Length,
     /// The stalk length.
-    Ls: usize,  
+    Ls: Length,  
     /// The tail length.
-    Lt: usize, 
+    Lt: Length, 
     /// The spins themselves (Change This)
     ///S: Vec<i8>, (Disregard this)   
-    /// The Spring Constants
-    kb: 
-    kub:
+    /// The unbinding constant
+    kub: usize,
+    /// The equilbrium Angles
+    eqb: usize,
+    eqmpre: usize,
+    eqmpost: usize,
+    eqt: usize,
 
 
     /// The last change we made (and might want to undo).
@@ -45,18 +52,36 @@ impl From<DyneinParams> for Dynein {
     fn from(params: DyneinParams) -> Dynein {
         let mut dynein = Dynein {
             E: Energy::new(0.),
-            N: params.N,
-            S: vec![1; params.N*params.N],
+            nm: params.nm,
+            fm: params.fm,
+            initd: params.initd,
+           //S: vec![1; params.N*params.N],
+            Ls = 20.75,     //nm
+            Lt = 23,        //nm
+            kub = 30,       //s^-1
+            eqb = 120,      
+            eqmpre = 197,
+            eqmpost = 242,
+            eqt = 0,
+
             possible_change: None,
         };
-        assert!(dynein.N > 1); // otherwise, we are our own neighbor!
+        //assert!(dynein.N > 1); // otherwise, we are our own neighbor!
         let mut rng = ::rng::MyRng::from_u64(10137);
-        for s in dynein.S.iter_mut() {
+        /*for s in dynein.S.iter_mut() {
             *s = *rng.choose(&[-1,1]).unwrap();
-        }
+        }*/
         dynein.E = dynein.compute_energy();
         dynein
     }
+}
+
+impl Dynien {
+    fn unbinding_rate(%self) -> f64 {
+        self.kub * E.powf(self.b*(self.nm-self.eqmpre)) // This is wrong, b = 1/(Boltzmann Constant * T) ?
+    }
+
+    fn  
 }
 
 impl System for Dynein {
@@ -65,6 +90,7 @@ impl System for Dynein {
     }
     fn compute_energy(&self) -> Energy {
         let mut e: Energy = units::EPSILON*0.0;
+        /*
         for i1 in 0 .. self.N {
             for j1 in 0 .. self.N {
                 let j2 = (j1 + 1) % self.N;
@@ -77,7 +103,7 @@ impl System for Dynein {
                 // neighbor_tot += self.S[i1 + j2*self.N];
                 e += neighbor_tot as f64 * Energy::new(self.S[i1+j1*self.N] as f64);
             }
-        }
+        }*/
         e
     }
     fn delta_energy(&self) -> Option<Energy> {
@@ -96,6 +122,7 @@ impl ConfirmSystem for Dynein {
 
 impl MovableSystem for Dynein {
     fn plan_move(&mut self, rng: &mut MyRng, _: Length) -> Option<Energy> {
+        /*
         let i = rng.gen_range(0, self.N);
         let j = rng.gen_range(0, self.N);
 
@@ -110,6 +137,7 @@ impl MovableSystem for Dynein {
         let e = self.E - neighbor_tot as f64 * self.S[i+j*self.N] as f64 * Energy::new(2.0);
         self.possible_change = Some((i+j*self.N, e));
         Some(e)
+        */
     }
 }
 
@@ -136,3 +164,4 @@ fn energy_works() {
         energy_works_with_N(n);
     }
 }
+
