cda3e6a4761c2d03755723ada0608af5bdde01f7
trotteca <trotteca@wngr412-unix24.science.oregonstate.edu>
2019-03-11 09:57:21 -0700

    Fixed plotting script for mulitple 3x3x3

--- energy-number/samc-3x3x3.sh
+++ energy-number/samc-3x3x3.sh
@@ -7,10 +7,10 @@ set -ev
 
 hostname
 
-nice -19 ../target/release/energy-number-mc --samc-t0 1e8 --save-as samc-t0-1e8-3x3x3.yaml --addremove-probability 0.2  --cell-volume 103.759 --movie-time "10^(1/4)" --well-width 1.3 --movie-name samc-t0-1e8-3x3x3.movie --save-time 0.5 --translation-scale 0.05 2> samc-t0-1e8-3x3x3.out &
+nice -19 ../target/release/energy-number-mc --samc-t0 1e8 --save-as samc-t0-1e8-3x3x3.yaml --addremove-probability 0.2  --cell-volume 103.759 --movie-time "10^(1/4)" --well-width 1.3 --movie-name samc-t0-1e8-3x3x3.movie --save-time 0.5 --translation-scale 0.05 > samc-t0-1e8-3x3x3.out &
 
-nice -19 ../target/release/energy-number-mc --samc-t0 1e5 --save-as samc-t0-1e5-3x3x3.yaml --addremove-probability 0.2  --cell-volume 103.759 --movie-time "10^(1/4)" --well-width 1.3 --movie-name samc-t0-1e5-3x3x3.movie --save-time 0.5 --translation-scale 0.05 2> samc-t0-1e5-3x3x3.out &
+nice -19 ../target/release/energy-number-mc --samc-t0 1e5 --save-as samc-t0-1e5-3x3x3.yaml --addremove-probability 0.2  --cell-volume 103.759 --movie-time "10^(1/4)" --well-width 1.3 --movie-name samc-t0-1e5-3x3x3.movie --save-time 0.5 --translation-scale 0.05 > samc-t0-1e5-3x3x3.out &
 
-nice -19 ../target/release/energy-number-mc --samc-t0 1e9 --save-as samc-t0-1e9-3x3x3.yaml --addremove-probability 0.2  --cell-volume 103.759 --movie-time "10^(1/4)" --well-width 1.3 --movie-name samc-t0-1e9-3x3x3.movie --save-time 0.5 --translation-scale 0.05 2> samc-t0-1e9-3x3x3.out &
+nice -19 ../target/release/energy-number-mc --samc-t0 1e9 --save-as samc-t0-1e9-3x3x3.yaml --addremove-probability 0.2  --cell-volume 103.759 --movie-time "10^(1/4)" --well-width 1.3 --movie-name samc-t0-1e9-3x3x3.movie --save-time 0.5 --translation-scale 0.05 > samc-t0-1e9-3x3x3.out &
 
 nice -19 ../target/release/energy-number-mc --samc-t0 1e6 --save-as samc-t0-1e6-3x3x3.yaml --addremove-probability 0.2  --cell-volume 103.759 --movie-time "10^(1/4)" --well-width 1.3 --movie-name samc-t0-1e6-3x3x3.movie --save-time 0.5 --translation-scale 0.05
