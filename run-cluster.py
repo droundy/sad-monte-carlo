20643f654c19ea242e698ab03e144c7074f3dfe2
David Roundy <roundyd@physics.oregonstate.edu>
2019-10-07 12:00:51 -0700

    add utility script to submit LJ cluster calculations (very crude so far)

---
+++ run-cluster.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys, os
+
+N = int(sys.argv[1])
+maxrad = int(N**(1/3) + 1.5)
+
+n_radial = maxrad*50+1
+
+# Need to also teach this to use different algorithms
+
+cmd = "rm -f lj-{N}.* && cargo build --release && rq run -J lj-{N} target/release/lj-cluster --N {N} --max-allowed-energy 0 --sad-min-T 0.1 --translation-scale 0.1 --energy-bin 0.1 --save-as lj-{N}.yaml --movie-time '10^(1/4)' --save-time 0.5 --n-radial {n_radial} --radius {maxrad}".format(N=N, maxrad=maxrad, n_radial=n_radial)
+print(cmd)
+#os.system(cmd)
