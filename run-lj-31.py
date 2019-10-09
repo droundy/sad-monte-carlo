06a93c178d5427621e5ac0808b065cb03e8d9268
David Roundy <roundyd@physics.oregonstate.edu>
2019-10-09 08:20:46 -0700

    new lj-31 script

---
+++ run-lj-31.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys, os
+
+N = 31
+maxrad = 3
+
+n_radial = maxrad*50+1
+
+# Need to also teach this to use different algorithms
+
+cmd = "rq cancel lj-{N} && cargo build --release && rm -f lj-{N}.* && rq run -J lj-{N} target/release/lj-cluster --N {N} --max-allowed-energy 0 --sad-min-T 0.02 --translation-scale 0.1 --energy-bin 0.05 --save-as lj-{N}.yaml --movie-time '10^(1/4)' --save-time 0.5 --n-radial {n_radial} --radius {maxrad}".format(N=N, maxrad=maxrad, n_radial=n_radial)
+print(cmd)
+os.system(cmd)
