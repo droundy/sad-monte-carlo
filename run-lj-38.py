1c177dfc2acf69643a9d016f1fd46da428267507
David Roundy <droundy@bennet.physics.oregonstate.edu>
2019-10-13 15:27:09 -0700

    start working on nicer file naming for lj clusters

--- run-lj-38.py
+++ run-lj-38.py
@@ -5,10 +5,15 @@ import sys, os
 N = 38
 maxrad = 3.5
 
+dE = 0.06
+minT = 0.01
+
 n_radial = maxrad*50+1
+
+froot = f"lj{N}-de{dE}-minT{minT}-sad"
 
 # Need to also teach this to use different algorithms
 
-cmd = "rq cancel lj-{N} && cargo build --release && rm -f lj-{N}.* && rq run -J lj-{N} target/release/lj-cluster --N {N} --max-allowed-energy 0 --sad-min-T 0.01 --translation-scale 0.1 --energy-bin 0.04 --save-as lj-{N}.yaml --movie-time '10^(1/4)' --save-time 0.5 --n-radial {n_radial} --radius {maxrad}".format(N=N, maxrad=maxrad, n_radial=n_radial)
+cmd = f"rq cancel {froot} && cargo build --release && rq run -J {froot} target/release/lj-cluster --N {N} --max-allowed-energy 0 --sad-min-T {minT} --translation-scale 0.1 --energy-bin {dE} --save-as {froot}.yaml --movie-time '10^(1/4)' --save-time 1 --n-radial {n_radial} --radius {maxrad}"
 print(cmd)
 os.system(cmd)
