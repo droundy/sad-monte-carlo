f0c9a4b840c48e0e98d92dd79058b5689093fc5e
David Roundy <roundyd@physics.oregonstate.edu>
2019-11-01 09:35:35 -0700

    start a "big" benchmark with a larger radius, to see what our cavity is really doing

--- big-lj-31.sh
+++ big-lj-31.sh
@@ -16,6 +16,8 @@ else
     ## ------ RUN ON A CONSTRAINED ENERGY RANGE ------ ## # The groundstate is -133.586422
     rq run -R -J 'big-lj-sad-0.01-minT-0.01' target/release/lj-cluster --N 31 --max-allowed-energy=0 --translation-scale 0.05 --movie-time '10^(1/8)' --save-time 2 --radius 5 --energy-bin 0.01 --save-as big-lj-sad-0.01-minT-0.01.yaml --sad-min-T 0.01
 
+    rq run -R -J 'big-lj-benchmark' target/release/lj-cluster --N 31 --min-allowed-energy=-133.53 --max-allowed-energy=-110 --translation-scale 0.03 --movie-time '10^(1/8)' --save-time 1 --radius 5 --energy-bin 0.001 --save-as big-lj-benchmark.yaml --Inv-t-WL
+
     # rq run --max-output 30 -R -J 'big-lj-sad-0.01-minT-0.01-bigmax' target/release/lj-cluster --N 31 --max-allowed-energy=200 --translation-scale 0.05 --movie-time '10^(1/8)' --movie-start 1e8 --save-time 2 --radius 5 --energy-bin 0.01 --save-as big-lj-sad-0.01-minT-0.01-bigmax.yaml --sad-min-T 0.01
 
     # rq run -R -J 'big-lj-sad-0.01-minT-0.02' target/release/lj-cluster --N 31 --max-allowed-energy=0 --translation-scale 0.05 --movie-time '10^(1/8)' --save-time 2 --radius 5 --energy-bin 0.01 --save-as big-lj-sad-0.01-minT-0.02.yaml --sad-min-T 0.02
