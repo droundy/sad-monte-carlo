aa9ed3caa94f49f8d13ce9ee98df1012a6501a72
vandegja <vandegja@wngr412-unix15.science.oregonstate.edu>
2019-11-14 11:27:03 -0800

    new shells and working on Grand_N

--- number/samc-2x2x2-T1.0-t0-1e7.sh
+++ number/samc-2x2x2-T1.0-t0-1e7.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
-#SBATCH --output samc-2x2x2-T1.0-t0-1e7.out
+#SBATCH --output samc-V40-T1.0-t0-1e9.out
 
 set -ev
 
 hostname
-nice -19 ../target/release/number-mc --T 1 --samc-t0 1e7 --save-as samc-2x2x2-T1.0-t0-1e7.yaml --acceptance-rate 0.5 --cell-volume '4*2^3*pi/6/0.545' --movie-time "10^(1/4)" --well-width 1.5
+nice -19 ../target/release/number-mc --T 1 --samc-t0 1e9 --save-as samc-V40-T1.0-t0-1e9.yaml --acceptance-rate 0.5 --cell-volume '4*(2.1834)^3*pi/6/0.545' --movie-time "10^(1/4)" --well-width 1.5
+nice -19 ../target/release/number-mc --T 1 --samc-t0 1e8 --save-as samc-V40-T1.0-t0-1e8.yaml --acceptance-rate 0.5 --cell-volume '4*(2.1834)^3*pi/6/0.545' --movie-time "10^(1/4)" --well-width 1.5
+nice -19 ../target/release/number-mc --T .9 --samc-t0 1e9 --save-as samc-V40-T0.9-t0-1e9.yaml --acceptance-rate 0.5 --cell-volume '4*(2.1834)^3*pi/6/0.545' --movie-time "10^(1/4)" --well-width 1.5
+nice -19 ../target/release/number-mc --T .9 --samc-t0 1e8 --save-as samc-V40-T0.9-t0-1e8.yaml --acceptance-rate 0.5 --cell-volume '4*(2.1834)^3*pi/6/0.545' --movie-time "10^(1/4)" --well-width 1.5
+nice -19 ../target/release/number-mc --T 1.1 --samc-t0 1e9 --save-as samc-V40-T1.1-t0-1e9.yaml --acceptance-rate 0.5 --cell-volume '4*(2.1834)^3*pi/6/0.545' --movie-time "10^(1/4)" --well-width 1.5
+nice -19 ../target/release/number-mc --T 1.1 --samc-t0 1e8 --save-as samc-V40-T1.1-t0-1e8.yaml --acceptance-rate 0.5 --cell-volume '4*(2.1834)^3*pi/6/0.545' --movie-time "10^(1/4)" --well-width 1.5
