24c5ab1d439ecc089be7c10a8e31aeea10aa270c
David Roundy <roundyd@physics.oregonstate.edu>
2020-03-11 12:35:20 -0700

    tweak wca-p-excess plotting

--- plotting/wca-p-excess.py
+++ plotting/wca-p-excess.py
@@ -34,7 +34,6 @@ my_energy={} #the energies
 my_entropy={} #entropies[t][i] -> entropy at time t, energy index i
 my_pexc_tot={} #excess pressure at energy index i
 my_count={} #count of each excess pressure - hence at energy index i
-my_pressure={} #my_pressure[t][i] -> system's pressure at time t, energy index i
 my_t={} #the times 't'
 
 
@@ -122,39 +121,51 @@ def calc_ideal_p(t, i):
         return args.density * Kb * my_temp(t,i)
     return T #whichever inf it was; +ve or -ve
 
+read_data(args.cbor)
+
 #function assumes at a given index i, my_energy[i] corresponds with my_entropy[i]
 #as well as my_pexc_tot[i] and my_count[i]
-def calc_pressure():   #calculate pressure and populate the my_pressure array
-    #given P = Pexcess + Pideal
-    global my_pressure
-    for t in range(0, len(my_entropy)):
-        my_pressure[t]={}
-        for i in range(0, len(my_energy)):
-            try:
-                my_pressure[t][i] = float(my_pexc_tot[i]) / float(my_count[i]) #p_excess
-            except ZeroDivisionError:
-                if my_pexc_tot[i] < 0:
-                    my_pressure[t][i] = -float('inf')
-                else:
-                    my_pressure[t][i] = float('inf')
-                continue
-            p_ideal = calc_ideal_p(t, i)
-            if p_ideal!=float('inf') and p_ideal!=-float('inf'):
-                my_pressure[t][i] += calc_ideal_p(t, i) #p_ideal
-            else:
-                my_pressure[t][i] = p_ideal #whatever inf it is; +ve or -ve
 
-read_data(args.cbor)
-calc_pressure()
+#calculate pressure and populate the my_pressure array
+#given P = Pexcess + Pideal
+my_pressure={} #my_pressure[t][i] -> system's pressure at time t, energy index i
+for t in range(0, len(my_entropy)):
+    my_pressure[t] = np.zeros_like(my_energy)
+    for i in range(0, len(my_energy)):
+        try:
+            my_pressure[t][i] = my_pexc_tot[i] / my_count[i] #p_excess
+        except ZeroDivisionError:
+            if my_pexc_tot[i] < 0:
+                my_pressure[t][i] = -float('inf')
+            else:
+                my_pressure[t][i] = float('inf')
+            continue
+        p_ideal = calc_ideal_p(t, i)
+        print('p_exc', my_pressure[t][i])
+        print('p_ideal', p_ideal)
+        if p_ideal!=float('inf') and p_ideal!=-float('inf'):
+            my_pressure[t][i] += calc_ideal_p(t, i) #p_ideal
+        else:
+            my_pressure[t][i] = p_ideal #whatever inf it is; +ve or -ve
+
 
 plt.ion()
 for i in range(0, len(my_pressure)):
+    plt.figure('pressure')
     plt.clf()
-    plt.xlabel('Energy\t(E)') #need proper symbols
-    plt.ylabel('Pressure\t(P)')
+    plt.xlabel('Energy (E)') #need proper symbols
+    plt.ylabel('Pressure (P)')
     plt.title('t = ' + str(my_t[i]))
-    plt.plot(my_energy, np.array( list(my_pressure[i].keys()) ) )
-    if i!=len(my_pressure)-1:
-        plt.pause(0.6)
-    else:
-        plt.show(block=True)
+    print(my_pressure[i])
+    plt.plot(my_energy, my_pressure[i])
+
+    plt.figure('entropy')
+    plt.clf()
+    plt.xlabel('Energy (E)') #need proper symbols
+    plt.ylabel('Entropy (S)')
+    plt.title('t = ' + str(my_t[i]))
+    plt.plot(my_energy, my_entropy[i])
+
+    plt.pause(0.6)
+
+plt.show()
