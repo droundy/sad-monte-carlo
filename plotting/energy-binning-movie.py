b75cb0d9ab1b0635d35bb14e7013a9a158ad7851
David Roundy <roundyd@physics.oregonstate.edu>
2020-03-21 05:56:08 -0700

    start a script to plot using new code

---
+++ plotting/energy-binning-movie.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import yaml, sys, argparse, cbor, glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+def latex_float(x):
+    exp = int(np.log10(x*1.0))
+    if abs(exp) > 2:
+        x /= 10.0**exp
+        if ('%.1g' % x) == '1':
+            return r'10^{%.0f}' % (exp)
+        return r'%.1g\times10^{%.0f}' % (x, exp)
+    else:
+        return '%g' % x
+
+parser = argparse.ArgumentParser(description="create movie and graphs for energy histogram data")
+parser.add_argument('--minT', action='store', type=float, default=0.005,
+                    help = "the minimum temperature of interest")
+parser.add_argument('--maxT', action='store', type=float, default=2.0,
+                    help = "the maximum temperature of interest")
+parser.add_argument('--match-energy', action='store', type=float,
+                    help = "the energy at which we want to normalize the entropy")
+parser.add_argument('dirname', nargs='*',
+                    help = 'the names of some yaml files')
+args = parser.parse_args()
+
+allcolors = list(reversed(['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple',
+                           'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan',
+                           'xkcd:lightblue', 'xkcd:puke', 'xkcd:puce', 'xkcd:turquoise']))
+
+class MC:
+    """ The state of a Monte Carlo simulation """
+    def __init__(self, filename):
+        with open(filename, 'rb') as f:
+            self.data = cbor.load(f)
+
+assert(len(args.dirname)==1) # fix this later
+for f in glob.glob(args.dirname[0]+'/*.cbor'):
+    print(f)
+    mc = MC(f)
+
+plt.show()
