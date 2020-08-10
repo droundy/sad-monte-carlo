7c56ea804672b99a8ad70feaeb0c91810fd6ea0e
David Roundy <daveroundy@gmail.com>
2020-08-10 12:25:48 -0700

    tweak to grand-transposed run script rq parameters

--- grand-transposed/run.sh
+++ grand-transposed/run.sh
@@ -4,10 +4,17 @@ set -ev
 
 cargo build --release --bin wca-grand-transposed --bin lj-grand-transposed --bin erfinv-grand-transposed
 
+echo default to run with two cores, a high upper bound on output file size, and as a restartable job
+RQ=(rq run -c 2 --max-output 20 -R)
+
 GENERIC=(--f 0.5 --translation-scale 0.05 --movie-time "10^(1/4)" --save-time 1)
 
-rq run -R -J wca-1 -- ../target/release/wca-grand-transposed --save-as wca-1.cbor --swap-time 1 --max-N 256 --cell-volume 100 --min-T 0.5 ${GENERIC[@]}
+# ${RQ[@]} -J wca-1 -- ../target/release/wca-grand-transposed --save-as wca-1.cbor --swap-time 1 --max-N 256 --cell-volume 100 --min-T 0.5 ${GENERIC[@]}
 
-# rq run -R -J lj-1 -- ../target/release/lj-grand-transposed --save-as lj-1.cbor --swap-time 1 --radius 4 --max-N 38 --min-T 0.01 ${GENERIC[@]}
+${RQ[@]} -J lj-1 -- ../target/release/lj-grand-transposed --save-as lj-1.cbor --swap-time 1 --radius 4 --max-N 38 --min-T 0.01 ${GENERIC[@]}
 
-# rq run -R -J erfinv-1 -- ../target/release/erfinv-grand-transposed --save-as erfinv-1.cbor --swap-time 1 --mean-energy 0 --min-T 0.01 --max-N 10  ${GENERIC[@]}
+# ${RQ[@]} -J erfinv-1 -- ../target/release/erfinv-grand-transposed --save-as erfinv-1.cbor --swap-time 1 --mean-energy 0 --min-T 0.01 --max-N 10  ${GENERIC[@]}
+
+sleep 5
+
+tail -f *.out
