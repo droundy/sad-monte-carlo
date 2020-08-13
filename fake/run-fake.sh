8b8c43f544f5c5f553d544a05352a6f1b9fd0469
David Roundy <daveroundy@gmail.com>
2020-08-13 09:55:40 -0700

    make run-fake run with several different sad widths, and with a --max-iter 1e10 so it will finish

--- fake/run-fake.sh
+++ fake/run-fake.sh
@@ -6,17 +6,19 @@ cargo build --release --bin fake-transposed --bin fake-binning --bin erfinv-tran
 
 ERFINV=(../target/release/erfinv-transposed --translation-scale 0.05 --movie-time "10^(1/4)" --mean-energy 0 --N 10)
 
-RUN=(../target/release/fake-transposed --translation-scale 0.05 --movie-time "10^(1/4)")
+RUN=(../target/release/fake-transposed --translation-scale 0.05 --movie-time "10^(1/4)" --max-iter 1e10)
 
-SAD=(../target/release/fake-binning --translation-scale 0.05 --movie-time "10^(1/4)" --histogram-bin 0.01)
+SAD=(../target/release/fake-binning --translation-scale 0.05 --movie-time "10^(1/4)" --max-iter 1e10)
 
 rq run -R -J t-erfinv -- ${ERFINV[@]//}  --save-as t-erfinv.yaml --min-T 0.1 --f '0.5'
 
-rq run -R -J sad-gaussian-0.01 -- ${SAD[@]//} --gaussian-sigma 0.1 --save-as sad-gaussian-0.01.yaml --sad-min-T 0.01
+for de in 0.01 0.1 1.0; do
+  rq run -R -J sad-gaussian-$de -- ${SAD[@]//} --histogram-bin $de --gaussian-sigma 0.1 --save-as sad-gaussian-$de.yaml --sad-min-T 0.01
 
-rq run -R -J sad-linear-0.01 --  ${SAD[@]//} --linear --save-as sad-linear-0.01.yaml --sad-min-T 0.01
+  rq run -R -J sad-linear-$de --  ${SAD[@]//} --histogram-bin $de --linear --save-as sad-linear-$de.yaml --sad-min-T 0.01
 
-rq run -R -J sad-quadratic-0.01 -- ${SAD[@]//}  --quadratic-dimensions 3 --save-as sad-quadratic-0.01.yaml --sad-min-T 0.01
+  rq run -R -J sad-quadratic-$de -- ${SAD[@]//} --histogram-bin $de --quadratic-dimensions 3 --save-as sad-quadratic-$de.yaml --sad-min-T 0.01
+done
 
 rq run -R -J t-gaussian -- ${RUN[@]//}  --gaussian-sigma 0.1 --save-as t-gaussian.yaml --min-T 0.01 --f '0.5'
 
