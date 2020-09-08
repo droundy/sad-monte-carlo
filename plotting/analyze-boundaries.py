cc56ee71a5e42c59ea0070a0746eb7f493d50ff4
David Roundy <daveroundy@gmail.com>
2020-09-08 09:41:33 -0700

    fix normalization of gaussian plot

--- plotting/analyze-boundaries.py
+++ plotting/analyze-boundaries.py
@@ -21,7 +21,7 @@ def linear_density_of_states(E):
 def quadratic_density_of_states(E):
     return 1.5*np.sqrt(E)*np.heaviside(E, 0)*np.heaviside(1-E, 0)
 def gaussian_density_of_states(E):
-    return (np.pi*(sigma**3)*np.sqrt(32*np.log(-1/E))) / -E
+    return (np.pi*(sigma**3)*np.sqrt(32*np.log(-1/E))) / -E / (4*np.pi/3)
 def other_density_of_states(E):
     return np.zeros_like(E)
 def erfinv_density_of_states(E):
