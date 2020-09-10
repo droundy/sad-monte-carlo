2524ad24491c75648075292a99e9902d925b3489
David Roundy <daveroundy@gmail.com>
2020-09-10 12:39:45 -0700

    teach run-fake.sh to use the new replicas MC... still need to parse the output

--- fake/run-fake.sh
+++ fake/run-fake.sh
@@ -2,13 +2,23 @@
 
 set -ev
 
-cargo build --release --bin transposed --bin binning
+cargo build --release --bin transposed --bin binning --bin replicas
 
 ERFINV=(../target/release/transposed --translation-scale 0.05 --movie-time "10^(1/4)" --fake-erfinv-mean-energy 0 --fake-erfinv-N 10 --max-iter 1e10)
 
-RUN=(../target/release/transposed --translation-scale 0.05 --movie-time "10^(1/4)" --max-iter 1e10)
+TRA=(../target/release/transposed --translation-scale 0.05 --movie-time "10^(1/4)" --max-iter 1e10)
+
+REP=(../target/release/replicas --movie-time "10^(1/4)" --max-iter 1e10)
 
 SAD=(../target/release/binning --translation-scale 0.05 --movie-time "10^(1/4)" --max-iter 1e10)
+
+rq run -R -J r-erfinv -- ${REP[@]//} --save-as r-erfinv.yaml --min-T 0.01 --fake-erfinv-mean-energy 0 --fake-erfinv-N 10
+
+rq run -R -J r-gaussian -- ${REP[@]//} --save-as r-gaussian.yaml --min-T 0.01 --fake-gaussian-sigma 0.1
+
+rq run -R -J r-linear -- ${REP[@]//} --save-as r-linear.yaml --min-T 0.01 --fake-linear
+
+rq run -R -J r-quadratic -- ${REP[@]//} --save-as r-quadratic.yaml --min-T 0.001 --fake-quadratic-dimensions 3
 
 rq run -R -J t-erfinv -- ${ERFINV[@]//}  --save-as t-erfinv.yaml --min-T 0.01 --f '0.5'
 
@@ -24,11 +34,11 @@ for de in 0.001 0.01 0.1; do
   rq run -R -J sad-quadratic-$de -- ${SAD[@]//} --histogram-bin $de --fake-quadratic-dimensions 3 --save-as sad-quadratic-$de.yaml --sad-min-T 0.001
 done
 
-rq run -R -J t-gaussian -- ${RUN[@]//}  --fake-gaussian-sigma 0.1 --save-as t-gaussian.yaml --min-T 0.01 --f '0.5'
+rq run -R -J t-gaussian -- ${TRA[@]//}  --fake-gaussian-sigma 0.1 --save-as t-gaussian.yaml --min-T 0.01 --f '0.5'
 
-rq run -R -J t-linear --  ${RUN[@]//} --fake-linear --save-as t-linear.yaml --min-T 0.01 --f '0.5'
+rq run -R -J t-linear --  ${TRA[@]//} --fake-linear --save-as t-linear.yaml --min-T 0.01 --f '0.5'
 
-rq run -R -J t-quadratic -- ${RUN[@]//}  --fake-quadratic-dimensions 3 --save-as t-quadratic.yaml --min-T 0.001 --f '0.5'
+rq run -R -J t-quadratic -- ${TRA[@]//}  --fake-quadratic-dimensions 3 --save-as t-quadratic.yaml --min-T 0.001 --f '0.5'
 
 sleep 5
 
