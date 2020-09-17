23d776c1420af84fa4caf6482f08eec3ca166e20
legendMK13 <kangethn@oregonstate.edu>
2020-09-17 09:57:17 +0300

    fix command line flags

--- fake/run-fake.sh
+++ fake/run-fake.sh
@@ -20,7 +20,7 @@ rq run -c all --max-output=30 -R -J r-linear -- ${REP[@]//} --save-as r-linear.y
 
 rq run -c all --max-output=30 -R -J r-quadratic -- ${REP[@]//} --save-as r-quadratic.yaml --min-T 0.001 --fake-quadratic-dimensions 3
 
-rq run -c all --max-output=30 -R -J r-pieces -- ${REP[@]//} --save-as r-pieces.yaml --min-T 0.001 --a 2.0 --b 4.0 --e1 1.0 --e2 3.0
+rq run -c all --max-output=30 -R -J r-pieces -- ${REP[@]//} --save-as r-pieces.yaml --min-T 0.001 --fake-pieces-a 2.0 --fake-pieces-b 4.0 --fake-pieces-e1 1.0 --fake-pieces-e2 3.0
 
 rq run -R -J t-erfinv -- ${ERFINV[@]//}  --save-as t-erfinv.yaml --min-T 0.01 --f '0.5'
 
@@ -35,7 +35,7 @@ for de in 0.001 0.01 0.1; do
 
   rq run -R -J sad-quadratic-$de -- ${SAD[@]//} --histogram-bin $de --fake-quadratic-dimensions 3 --save-as sad-quadratic-$de.yaml --sad-min-T 0.001
   
-  rq run -R -J sad-pieces-$de -- ${SAD[@]//} --histogram-bin $de --a 2.0 --b 4.0 --e1 1.0 --e2 3.0 --save-as sad-pieces-$de.yaml --sad-min-T 0.001
+  rq run -R -J sad-pieces-$de -- ${SAD[@]//} --histogram-bin $de --fake-pieces-a 2.0 --fake-pieces-b 4.0 --fake-pieces-e1 1.0 --fake-pieces-e2 3.0 --save-as sad-pieces-$de.yaml --sad-min-T 0.001
 done
 
 rq run -R -J t-gaussian -- ${TRA[@]//}  --fake-gaussian-sigma 0.1 --save-as t-gaussian.yaml --min-T 0.001 --f '0.5'
@@ -44,7 +44,7 @@ rq run -R -J t-linear --  ${TRA[@]//} --fake-linear --save-as t-linear.yaml --mi
 
 rq run -R -J t-quadratic -- ${TRA[@]//}  --fake-quadratic-dimensions 3 --save-as t-quadratic.yaml --min-T 0.001 --f '0.5'
 
-rq run -R -J t-pieces -- ${TRA[@]//}  --a 2.0 --b 4.0 --e1 2.0 --e2 3.0 --save-as t-pieces.yaml --min-T 0.001 --f '0.5'
+rq run -R -J t-pieces -- ${TRA[@]//}  --fake-pieces-a 2.0 --fake-pieces-b 4.0 --fake-pieces-e1 2.0 --fake-pieces-e2 3.0 --save-as t-pieces.yaml --min-T 0.001 --f '0.5'
 
 sleep 5
 
