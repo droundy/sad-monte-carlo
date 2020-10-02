3456d51fc67635dc339c34becf38314bfee40b6e
nelson kangethe <nelson@bingley.physics.oregonstate.edu>
2020-10-02 10:18:39 -0700

    created 'PIECES' variable

--- fake/run-fake.sh
+++ fake/run-fake.sh
@@ -12,6 +12,8 @@ REP=(../target/release/replicas --movie-time "10^(1/4)" --max-iter 1e10)
 
 SAD=(../target/release/binning --translation-scale 0.05 --movie-time "10^(1/4)" --max-iter 1e10)
 
+PIECES=( --fake-pieces-a 0.1 --fake-pieces-b 0.2 --fake-pieces-e1 1.0 --fake-pieces-e2 0.5)
+
 rq run -c all --max-output=30 -R -J r-erfinv -- ${REP[@]//} --save-as r-erfinv.yaml --min-T 0.01 --fake-erfinv-mean-energy 0 --fake-erfinv-N 10
 
 rq run -c all --max-output=30 -R -J r-gaussian -- ${REP[@]//} --save-as r-gaussian.yaml --min-T 0.001 --fake-gaussian-sigma 0.1
@@ -20,7 +22,7 @@ rq run -c all --max-output=30 -R -J r-linear -- ${REP[@]//} --save-as r-linear.y
 
 rq run -c all --max-output=30 -R -J r-quadratic -- ${REP[@]//} --save-as r-quadratic.yaml --min-T 0.001 --fake-quadratic-dimensions 3
 
-rq run -c all --max-output=30 -R -J r-pieces -- ${REP[@]//} --save-as r-pieces.yaml --min-T 0.001 --fake-pieces-a 2.0 --fake-pieces-b 4.0 --fake-pieces-e1 1.0 --fake-pieces-e2 3.0
+rq run -c all --max-output=30 -R -J r-pieces -- ${REP[@]//} --save-as r-pieces.yaml --min-T 0.001 ${PIECES[@]//}
 
 rq run -R -J t-erfinv -- ${ERFINV[@]//}  --save-as t-erfinv.yaml --min-T 0.01 --f '0.5'
 
@@ -35,7 +37,7 @@ for de in 0.001 0.01 0.1; do
 
   rq run -R -J sad-quadratic-$de -- ${SAD[@]//} --histogram-bin $de --fake-quadratic-dimensions 3 --save-as sad-quadratic-$de.yaml --sad-min-T 0.001
   
-  rq run -R -J sad-pieces-$de -- ${SAD[@]//} --histogram-bin $de --fake-pieces-a 2.0 --fake-pieces-b 4.0 --fake-pieces-e1 1.0 --fake-pieces-e2 3.0 --save-as sad-pieces-$de.yaml --sad-min-T 0.001
+  rq run -R -J sad-pieces-$de -- ${SAD[@]//} --histogram-bin $de ${PIECES[@]//} --save-as sad-pieces-$de.yaml --sad-min-T 0.001
 done
 
 rq run -R -J t-gaussian -- ${TRA[@]//}  --fake-gaussian-sigma 0.1 --save-as t-gaussian.yaml --min-T 0.001 --f '0.5'
@@ -44,7 +46,7 @@ rq run -R -J t-linear --  ${TRA[@]//} --fake-linear --save-as t-linear.yaml --mi
 
 rq run -R -J t-quadratic -- ${TRA[@]//}  --fake-quadratic-dimensions 3 --save-as t-quadratic.yaml --min-T 0.001 --f '0.5'
 
-rq run -R -J t-pieces -- ${TRA[@]//}  --fake-pieces-a 2.0 --fake-pieces-b 4.0 --fake-pieces-e1 2.0 --fake-pieces-e2 3.0 --save-as t-pieces.yaml --min-T 0.001 --f '0.5'
+rq run -R -J t-pieces -- ${TRA[@]//} ${PIECES[@]//} --save-as t-pieces.yaml --min-T 0.001 --f '0.5'
 
 sleep 5
 
