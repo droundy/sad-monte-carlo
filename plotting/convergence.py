e9fa5bf685cab328bebff19a465aff9ca218c355
David Roundy <roundyd@physics.oregonstate.edu>
2021-03-03 13:40:20 -0800

    remove unused code

--- plotting/convergence.py
+++ plotting/convergence.py
@@ -33,11 +33,15 @@ def beautiful_name(base):
     elif 'itwl-' == base[:5]:
         name += r'$t^{-1}$-WL '
         base = base[5:]
-    if base == 'erfinv':
+    if base == 'erfinv' or base == 'quadratic':
         name += ''
         base = ''
     elif base[:7] == 'erfinv-':
         base = base[7:]
+        name += rf' $\Delta E = {base}$'
+        base = ''
+    elif base[:10] == 'quadratic-':
+        base = base[10:]
         name += rf' $\Delta E = {base}$'
         base = ''
     return name + base
@@ -70,107 +74,6 @@ def piecewise_density_of_states(E):
         return (a**3 * np.sqrt(E+e1) / 2) + ( (b - (b - a)*np.sqrt(E/e2 + 1))**2 / (2*e2*np.sqrt(E/e2+1)) ) + ( (b + (b - a)*np.sqrt(E/e2 + 1))**2 / (2*e2*np.sqrt(E/e2+1)) )
     return 0
 piecewise_density_of_states = np.vectorize(piecewise_density_of_states)
-
-#The function needs to be callable
-
-def fn_entropy(S_i_1, E_i_1, E_i, lnw_i, S_i):
-    ''' This is the thing that should be zero '''
-    ''' Equation initially is:
-            W_i = e^coefficient * (inside) / denominator
-            
-            introducing ln simplifies to:
-                lnW_i = ln(coefficient*inside) - ln(denominator)
-                lnW_i = ln(coefficient) + ln(inside) - ln(denominator)
-                0 = ln(coefficient) + ln(inside) - ln(denominator) - lnW_i
-    '''
-    delta_E = E_i_1 - E_i
-    assert(delta_E>0)
-    delta_S = S_i_1 - S_i
-    S_0 = S_i_1
-    if abs(delta_S) < 1e-14:
-        return np.log(delta_E) + S_0 - lnw_i
-    else:
-        return np.log(delta_E) + S_0 - np.log((np.exp(delta_S)- 1)/delta_S) - lnw_i
-
-    denominator = np.log((S_i - S_i_1) / (E_i - E_i_1))
-    coefficient = (S_i*E_i - S_i_1*E_i_1) / (E_i - E_i_1)
-    inside = np.log(
-                    np.exp( E_i_1 * ((S_i - S_i_1) / (E_i - E_i_1)) )
-                    - np.exp( E_i * ((S_i - S_i_1) / (E_i - E_i_1)) ) 
-                    )
-    return coefficient + inside - denominator - lnw_i
-def optimize_bin_entropy(i, E_bounds, lnw, S_i):
-    #i is the bin whose entropy we are calculating
-    #print('solving E_i_1', E_bounds[i-1], 'E_i', E_bounds[i], 'lnw_i', lnw[i], 'S_i', S_i)
-    sol = optimize.root(fn_entropy, [0], args=(E_bounds[i-1], E_bounds[i], lnw[i], S_i))
-    # print('   goodness is', fn_entropy(sol.x[0], E_bounds[i-1], E_bounds[i], lnw[i], S_i),
-    #       'with entropy', sol.x[0])
-    # print('      delta S =', sol.x[0] - S_i, np.exp(sol.x[0] - S_i))
-    # print('      delta E =', E_bounds[i-1] - E_bounds[i])
-    # print('      S_0 =', S_i)
-    # print('      w =', np.exp(lnw[i]))
-    # print('      S estimate =', lnw[i] - np.log(E_bounds[i-1] - E_bounds[i]))
-    
-    # plt.figure('debugging')
-    # xxx = np.linspace(-10,100, 10000)
-    # answer = np.zeros_like(xxx)
-    # for j in range(len(xxx)):
-    #     answer[j] = fn_entropy(xxx[j], E_bounds[i-1], E_bounds[i], lnw[i], S_i)
-    # plt.plot(xxx, answer)
-    # plt.show()
-    return sol.x[0]
-
-def compute_entropy_given_Smin(Smin, energy_boundaries, lnw):
-    entropy_boundaries = np.zeros_like(energy_boundaries)
-    entropy_boundaries[-1] = Smin
-    for i in range(len(energy_boundaries)-1, 1, -1):
-        #print('solving for i =', i)
-        entropy_boundaries[i-1] = optimize_bin_entropy(i, energy_boundaries, lnw, entropy_boundaries[i])
-    return entropy_boundaries
-
-def entropy_boundary_badness(Smin, energy_boundaries, lnw):
-    entropy_boundaries = compute_entropy_given_Smin(Smin, energy_boundaries, lnw)
-    badness = 0
-    for i in range(len(energy_boundaries)-2):
-        de1 = energy_boundaries[i+1] - energy_boundaries[i]
-        de2 = energy_boundaries[i+2] - energy_boundaries[i+1]
-        dS1 = entropy_boundaries[i+1] - entropy_boundaries[i]
-        dS2 = entropy_boundaries[i+2] - entropy_boundaries[i+1]
-        badness += abs((dS2/de2 - dS1/de1)/(de2+de1))
-    return badness
-
-def optimize_entropy(energy_boundaries, lnw):
-    res = optimize.minimize_scalar(entropy_boundary_badness, args=(energy_boundaries, lnw))
-    Smin = res.x
-    return compute_entropy_given_Smin(Smin, energy_boundaries, lnw)
-
-def bisect_bin_entropy(i):
-    #i is the bin whose entropy we are calculating
-    return
-
-def fn_for_beta(x, meanE_over_deltaE):
-    if x < 1e-14:
-        return 0.5*x - x*meanE_over_deltaE
-    return x/(1-np.exp(-x)) - 1 - x*meanE_over_deltaE
-
-def find_beta_deltaE(meanE_over_deltaE):
-    # x = np.linspace(-100,100,10000)
-    # plt.plot(x, np.vectorize(fn_for_beta)(x, meanE_over_deltaE))
-    # plt.show()
-    if meanE_over_deltaE == 0:
-        x0 = 1e-6
-        x1 = -1e-6
-    else:
-        x0 = 2*meanE_over_deltaE
-        x1 = meanE_over_deltaE
-    sol = optimize.root_scalar(fn_for_beta, args=(meanE_over_deltaE), x0 = x0, x1 = x1)
-    # print(sol)
-    return sol.root
-
-def find_entropy_from_beta_and_lnw(beta, lnw, deltaE):
-    if abs(beta*deltaE) < 1e-14:
-        return lnw - np.log(deltaE)
-    return lnw - np.log(deltaE) - np.log((np.exp(beta*deltaE)-1)/(beta*deltaE))
 
 #Read Data
 moves = {}
