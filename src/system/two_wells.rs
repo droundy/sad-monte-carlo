ec92f429674b67c7c2781c37861a64f09e7cc764
unknown <henry.sprueill@gmail.com>
2021-05-15 18:07:14 -0700

    Finished implementing two-wells. Changed position related variables to f64 from Length

--- src/system/two_wells.rs
+++ src/system/two_wells.rs
@@ -3,10 +3,10 @@
 use super::*;
 
 use rand::prelude::*;
-/// The parameters needed to configure an fake model with N dimensions.
+/// The parameters needed to configure a fake model with N dimensions.
 ///
 /// These parameters are normally set via command-line arguments.
-#[derive(Serialize, Deserialize, Debug, AutoArgs, Clone)]
+#[derive(Serialize, Deserialize, Debug, AutoArgs, Clone)] //AutoArgs in incompatable with Vec<Length>
 #[allow(non_snake_case)]
 pub struct Parameters {
     /// the mean energy
@@ -18,9 +18,9 @@ pub struct Parameters {
     /// Height of the second well
     pub h_2: Energy,
     /// Radius of the first well
-    pub r_1: Length,
+    pub r_1: f64,
     /// Radius of the first well
-    pub r_2: Length,
+    pub r_2: f64,
     /// Center of the Second Well
     pub center_2: Vec<f64>,
 }
@@ -30,17 +30,17 @@ pub struct Parameters {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TwoWells {
     /// The state of the system
-    pub position: Vec<Length>,
+    pub position: Vec<f64>,
     /// The function itself
     pub parameters: Parameters,
     /// The last change we made (and might want to undo).
-    possible_change: Vec<Length>,
+    possible_change: Vec<f64>,
 }
 
 impl From<Parameters> for TwoWells {
     fn from(parameters: Parameters) -> TwoWells {
         TwoWells {
-            position: vec![Length::new(0.5); parameters.N],
+            position: vec![0.5; parameters.N],
             possible_change: Vec::new(),
             parameters,
         }
@@ -48,8 +48,24 @@ impl From<Parameters> for TwoWells {
 }
 
 impl TwoWells {
-    fn find_energy(&self, position: &[Length]) -> Energy {
-        self.parameters.h_1
+    fn find_energy(&self, position: &[f64]) -> Energy {
+        let mut d_1_squared = 0.;
+        let mut d_2_squared = 0.;
+
+        for i in 0..(self.parameters.N){
+            d_1_squared += position[i].powi(2);
+            d_2_squared += (position[i] - self.parameters.center_2[i]).powi(2);
+        }
+
+        let e_1 = self.parameters.h_1*(d_1_squared/ (self.parameters.r_1*self.parameters.r_1) - 1.);
+        let e_2 = self.parameters.h_2*(d_2_squared/ (self.parameters.r_2*self.parameters.r_2) - 1.);
+
+        if e_1 < e_2{
+            e_2
+        }
+        else{
+            e_1
+        }
     }
 }
 
@@ -62,7 +78,7 @@ impl System for TwoWells {
     }
     fn randomize(&mut self, rng: &mut MyRng) -> Energy {
         for x in self.position.iter_mut() {
-            *x = Length::new(rng.gen_range(-1.0, 1.0));
+            *x = rng.gen_range(-1.0, 1.0);
         }
         self.energy()
     }
@@ -86,8 +102,8 @@ impl MovableSystem for TwoWells {
         let i = rng.gen_range(0, self.position.len());
         self.possible_change = self.position.clone();
         let v: f64 = rng.sample(rand_distr::StandardNormal);
-        self.possible_change[i] += v * d;
-        if self.possible_change[i] >= Length::new(1.0) || self.possible_change[i] <= Length::new(-1.0) {
+        self.possible_change[i] += v * d.value_unsafe;
+        if self.possible_change[i] >= 1.0 || self.possible_change[i] <= -1.0 {
             return None;
         }
         Some(self.find_energy(&self.possible_change))
