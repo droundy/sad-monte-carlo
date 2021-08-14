047772820d077fb4a8b58572592cde0aaa36256a
David Roundy <roundyd@physics.oregonstate.edu>
2021-08-14 14:34:20 -0700

    compare simulation with exact result (revealing a known normalization issue)

--- two-wells/convergence.py
+++ two-wells/convergence.py
@@ -9,13 +9,15 @@ E = np.linspace(-system.h_small, 0, 10000)
 E = 0.5*(E[1:] + E[:-1])
 plt.plot(E, system.S(E), label='exact')
 
-for fname in glob.glob('z-*'+system.system+'*.cbor'):
+for fname in sorted(glob.glob('z-*'+system.system+'*.cbor')):
+    print(fname)
     base = fname[:-5]
 
-    energy_boundaries, mean_e, my_lnw, system, p_exc = compute.read_file(f)
+    energy_boundaries, mean_e, my_lnw, system, p_exc = compute.read_file(base)
     
     # Create a function for the entropy
     l_function, eee, sss = compute.linear_entropy(energy_boundaries, mean_e, my_lnw)
+    plt.plot(E, l_function(E), label=base)
 
 plt.legend()
 plt.show()
