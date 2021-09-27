2e2b96131ee517b41fc95c5a6e1e6306cf6073e4
David Roundy <daveroundy@gmail.com>
2021-09-27 12:06:38 -0700

    set up to run and plot histogram algorithms also for LJ31

--- lj/parse.sh
+++ lj/parse.sh
@@ -5,11 +5,11 @@ set -ev
 ../plotting/parse-replicas.py --reparse z-*.cbor
 ../plotting/parse-replicas.py z-*/*.cbor
 
-# ../plotting/parse-binning.py --reparse wl-*.cbor
-# ../plotting/parse-binning.py wl-*/*.cbor
+../plotting/parse-binning.py --reparse wl-*.cbor
+../plotting/parse-binning.py wl-*/*.cbor
 
-# ../plotting/parse-binning.py --reparse sad-*.cbor
-# ../plotting/parse-binning.py sad-*/*.cbor
+../plotting/parse-binning.py --reparse sad-*.cbor
+../plotting/parse-binning.py sad-*/*.cbor
 
-# ../plotting/parse-binning.py --reparse itwl-*.cbor
-# ../plotting/parse-binning.py itwl-*/*.cbor
+../plotting/parse-binning.py --reparse itwl-*.cbor
+../plotting/parse-binning.py itwl-*/*.cbor
