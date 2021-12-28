changes to parsing and corrected heat capacity calculation for tempering

plotting/parse-tempering.py
@@ -37,11 +37,8 @@ for fname in args.fname:
     print('done reading', fname)
 
 
-
+rs = []
 for r in data_loaded['replicas']:
-
-
-
     r_clean = dict()
     r_clean['accepted_swap'] = r['accepted_swap_count'] / (
                     r['rejected_swap_count'] + r['accepted_swap_count']
@@ -58,6 +55,17 @@ for r in data_loaded['replicas']:
 
     r_clean['T'] = r['T']
 
+    r_clean['Cv'] = r_clean['mean_E_squared'] - r_clean['mean_E']**2 /(r_clean['T']**2)
+
     print(r_clean)
+    rs.append(r_clean)
+
+cvs = []
+Ts = []
+for r in rs:
+    cvs.append(r['Cv'])
+    Ts.append(r['T'])
+
+np.savez('Cv.npz', T = Ts, Cv = cvs)
 
 

two-wells/generate-convergence.py
@@ -39,7 +39,7 @@ np.savez(system.name(), E=E,
                         correct_C=correct_C)
 
 paths = []
-for fname in sorted(glob.glob('*'+system.system+'*-lnw.dat')):
+for fname in sorted(glob.glob('itwl*'+system.system+'*-lnw.dat')):
     if not ('half-barrier' in fname):
         if 'sad' in fname:
             if True:#not( '0.01+0.001' in fname):
@@ -107,5 +107,5 @@ def generate_npz(fname):
 from multiprocessing import Pool
 
 if __name__ == '__main__':
-    with Pool(7) as p:
+    with Pool(8 ) as p:
         p.map(generate_npz, list(paths))